39be2753aa436ccb9cba86d438566df3913a80c1
Kantslo <gqancliani@gmail.com>
2024-04-07 15:56:50 +0400

    chore: organized main layout and page files

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import RegistrationModal from "@/components/authentication/registration-modal";
+
 export default function Home() {
-  return <main></main>;
+    return (
+        <main>
+            <RegistrationModal />
+        </main>
+    );
 }
